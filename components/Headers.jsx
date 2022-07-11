a945f7f996abe7cdbb7881cba7f9b28a99d7fb51
Sharath <sharathkrml@gmail.com>
2022-07-11 09:08:16 +0530

    added web3uikit connectBUtton

--- components/Headers.jsx
+++ components/Headers.jsx
@@ -1,49 +1,9 @@
-import { useMoralis } from "react-moralis"
-import { useEffect } from "react"
+import { ConnectButton } from "web3uikit"
 const Headers = () => {
-  const {
-    enableWeb3,
-    account,
-    isWeb3Enabled,
-    isWeb3EnableLoading,
-    Moralis,
-    deactivateWeb3,
-  } = useMoralis()
-  useEffect(() => {
-    if (isWeb3Enabled) return
-    if (window.localStorage.getItem("connected")) {
-      enableWeb3()
-    }
-  }, [isWeb3Enabled])
-
-  useEffect(() => {
-    Moralis.onAccountChanged((account) => {
-      console.log(`Account changed to ${account}`)
-      if (account == null) {
-        window.localStorage.removeItem("connected")
-        deactivateWeb3()
-      }
-    })
-  }, [])
-
   return (
     <div>
-      {account ? (
-        <div>
-          Conencted to {account.slice(0, 6)}...
-          {account.slice(account.length - 4)}
-        </div>
-      ) : (
-        <button
-          onClick={async () => {
-            await enableWeb3()
-            window.localStorage.setItem("connected", "injected")
-          }}
-          disabled={isWeb3EnableLoading}
-        >
-          Connect
-        </button>
-      )}
+      Decentralized Lottery
+      <ConnectButton moralisAuth={false} />
     </div>
   )
 }
